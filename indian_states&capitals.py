1c43c8037df20f7c3e7d5b594e3b66474e41c2ac
divya15038 <divyasharma1509ds@gmail.com>
2021-09-12 15:48:47 +0530

    Added class HashMap

--- indian_states&capitals.py
+++ indian_states&capitals.py
@@ -49,4 +49,32 @@ class LinkedList:
             else:
                 current_node = next_node
 
-
+class HashMap:
+    #initializing a HashMap
+    def __init__(self, array_size = None):
+        self.array_size = array_size
+        self.array = [LinkedList for i in range(array_size)]
+    #returns hash code for a key
+    def hash(self, key):
+        return sum(key.encode())
+    #returns array inex from hash code
+    def compress(self, hash_code):
+        return hash_code % self.array_size
+    #assigns key to an array index
+    def assign(self, key, value):
+        array_index = self.compress(self.hash(key))
+        new_node = Node([key, value])
+        list_at_index = self.array[array_index]
+        for item in list_at_index:
+            if item[0] == key:
+                item = [key, value]
+                return
+            list_at_index.append(new_node)
+    #returns value for a given key
+    def retrieve(self, key):
+        array_index = self.compress(self.hash(key))
+        list_at_index = self.array[array_index]
+        for element in list_at_index:
+            if element[0] == key:
+                return element[1]
+            return None
